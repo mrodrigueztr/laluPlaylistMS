41ae3c9f824781d1708616b644adf2dfdcbd4d7c
mrodrigueztr <mrodrigueztr@unal.edu.co>
2022-05-03 18:26:09 -0500

    put new songs into the list of playlist_songs

--- ProyectoArqui/lalu_playlist_ms/routes/lalu/playlist.route.js
+++ ProyectoArqui/lalu_playlist_ms/routes/lalu/playlist.route.js
@@ -71,6 +71,7 @@ router.get('/id/:playlist_id/songs', async(req,res) => {
 });
 
 router.post('/newPlaylist', async (req,res) => {
+    req.body.playlist_songs = []
     try {
         const newPlaylist =  await Playlist.create(req.body);
         return res.json({msage : 'Playlist Creada',data : newPlaylist});
@@ -93,10 +94,51 @@ router.put('/:playlist_id', async (req,res) => {
     }
 });
 
+router.put('/:playlist_id/updates/songs/:song_id', async (req,res) => {
+    try {
+        const playlist = await Playlist.findById(
+            req.params.playlist_id,
+        );
+
+        playlist.playlist_songs.push(req.params.song_id)
+        const newPlaylist = await Playlist.findByIdAndUpdate(
+            req.params.playlist_id,
+            playlist,
+            { new : true}
+        )
+        return res.json({msage : 'Cancion Agregada a la PLaylist',data : newPlaylist})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error : 'Ha ocurrido un error'});
+    }
+});
+
+router.put('/:playlist_id/delete/songs/:song_id', async (req,res) => {
+    try {
+        const playlist = await Playlist.findById(
+            req.params.playlist_id,
+        );
+        const index = playlist.playlist_songs.indexOf(req.params.song_id);
+        if(index !== -1){playlist.playlist_songs.splice(index,1)}
+        
+        const newPlaylist = await Playlist.findByIdAndUpdate(
+            req.params.playlist_id,
+            playlist,
+            { new : true}
+        )
+        return res.json({msage : 'Cancion Eliminada de la PLaylist',data : newPlaylist})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error : 'Ha ocurrido un error'});
+    }
+});
+
 router.delete('/:playlist_id', async (req,res) =>{
     try {
         const playlist = await Playlist.findByIdAndDelete(req.params.playlist_id);
-        return res.json({msage : 'Playlist Eliminada',data : playlist}); 
+        return res.json({msage : 'Playlist Actualizada',data : playlist}); 
     } catch (error) {
         console.log(error);
         return res.status(500).json({error : 'Ha ocurrido un error'});
