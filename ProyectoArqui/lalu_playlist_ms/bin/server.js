947c155045eb4bccb3916e86570b741027dcbf7b
Miguel Bolaños <mbolanoss@unal.edu.co>
2022-05-04 13:12:56 -0500

    Changed server port to 3000

--- ProyectoArqui/lalu_playlist_ms/bin/server.js
+++ ProyectoArqui/lalu_playlist_ms/bin/server.js
@@ -1,15 +1,14 @@
+const connectDB = require("../db/lalu_playlist_db");
+const app = require("../laluPlaylist");
 
-const connectDB = require('../db/lalu_playlist_db');
-const app = require('../laluPlaylist');
-
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB('mongodb+srv://mrodrigueztr:prueba@api-rest.ty8tf.mongodb.net/laluPlaylistsDb?retryWrites=true&w=majority');
-    app.listen(port, () =>
-      console.log(`Server is listening on port ...`)
+    await connectDB(
+      "mongodb+srv://mrodrigueztr:prueba@api-rest.ty8tf.mongodb.net/laluPlaylistsDb?retryWrites=true&w=majority"
     );
+    app.listen(port, () => console.log(`Server is listening on port ...`));
   } catch (error) {
     console.log(error);
   }
